0d6bef55dc2a12f1ba7b0df11191eff5746fa85c
brunovieira7 <brunovieira7@github.com>
2017-04-30 00:17:40 -0300

    intro screen

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
+			if (Input.touchCount > 0) {
+				if (Input.GetTouch (0).phase == TouchPhase.Began) {
+					Application.LoadLevel("FightScene");
+				}
+			}
+		} else if (Application.platform == RuntimePlatform.WindowsEditor) {
+			if (Input.GetMouseButtonDown (0)) {
+				Application.LoadLevel("FightScene");
+			}
+		}
+	}
+}
